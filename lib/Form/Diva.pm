9204a9216554305dd87849690a093e700fba2959
John Karr <brainbuz@cpan.org>
2014-07-11 06:34:35 +0000

    Import of BRAINBUZ/Form-Diva-0.05-TRIAL from CPAN.
    gitpan-cpan-distribution: Form-Diva
    gitpan-cpan-version:      0.05-TRIAL
    gitpan-cpan-path:         BRAINBUZ/Form-Diva-0.05-TRIAL.tar.gz
    gitpan-cpan-author:       BRAINBUZ
    gitpan-cpan-maturity:     developer

--- lib/Form/Diva.pm
+++ lib/Form/Diva.pm
@@ -3,7 +3,11 @@ use warnings;
 no warnings 'uninitialized';
 
 package Form::Diva;
-$Form::Diva::VERSION = '0.04';
+$Form::Diva::VERSION = '0.05'; # TRIAL
+# ABSTRACT: Generate HTML5 form label and input fields
+
+use Storable qw(dclone);
+
 sub new {
     my $class = shift;
     my $self  = {@_};
@@ -16,13 +20,47 @@ sub new {
     return $self;
 }
 
+sub clone {
+    my $self = shift ;
+    my $args = shift ;
+    my $new = {};
+    my $class = 'Form::Diva';
+    $new->{FormHash}    = dclone $self->{FormHash};
+    $new->{input_class} = $args->{input_class} || $self->{input_class};
+    $new->{label_class} = $args->{label_class} || $self->{label_class};
+    $new->{form_name}   = $args->{form_name}   || $self->{form_name};
+    if( $args->{neworder} ){
+            my @reordered = map { $new->{FormHash}->{$_} } @{$args->{neworder}};
+            $new->{FormMap} = \@reordered ;
+        }
+    else { $new->{FormMap} = dclone $self->{FormMap} ; }        
+    bless $new, $class ;
+    return $new ;
+}
+
+# so far diva hasn't needed the form name
+# sub form_name {
+#     my $self = shift ;
+#     return $self->{form_name};
+# }
+
+sub input_class {
+    my $self = shift ;
+    return $self->{input_class};
+}
+
+sub label_class {
+    my $self = shift ;
+    return $self->{label_class};
+}
+
 # specification calls for single letter shortcuts on all fields
 # these all need to expand to the long form.
 sub _expandshortcuts {
     my %DivaShortMap = (
         qw /
             n name t type i id e extra x extra l label p placeholder
-            d default v values c class/
+            d default v values c class /
     );
     my %DivaLongMap = map { $DivaShortMap{$_}, $_ } keys(%DivaShortMap);
     my $FormHash    = {};
@@ -104,7 +142,7 @@ sub _input {
     my %B     = $self->_field_bits( $field, $data );
     my $input = '';
     if ( $B{textarea} ) {
-        $input = qq|<TEXTAREA $B{name} $B{id}"
+        $input = qq|<TEXTAREA $B{name} $B{id}
         $B{input_class} $B{placeholder} $B{extra} >$B{rawvalue}</TEXTAREA>|;
     }
     else {
@@ -120,23 +158,28 @@ sub _input {
 # this needs to be implemented after data is being handled because
 # default is irrelevant if there is data.
 
-sub _radiocheck {            # field, input_class, data;
-    my $self        = shift;
-    my $field       = shift;
-    my $input_class = shift;
-    my $data        = shift;
-    my $output      = '';
-    my $extra       = $field->{extra} || "";
-    my $default     = $field->{default}
+sub _radiocheck {    # field, input_class, data;
+    my $self           = shift;
+    my $field          = shift;
+    my $data           = shift;
+    my $replace_fields = shift;
+    my $output         = '';
+    my $input_class    = $self->_class_input($field);
+    my $extra          = $field->{extra} || "";
+    my $default        = $field->{default}
         ? do {
         if   ($data) {undef}
         else         { $field->{default} }
         }
         : undef;
-    foreach my $val ( @{ $field->{values} } ) {
+    my @values
+        = $replace_fields
+        ? @{$replace_fields}
+        : @{ $field->{values} };    
+    foreach my $val ( @values ) {
         my ( $value, $v_lab ) = ( split( /\:/, $val ), $val );
         my $checked = '';
-        if    ( $data    eq $value ) { $checked = 'checked ' }
+        if    ( $data eq $value )    { $checked = 'checked ' }
         elsif ( $default eq $value ) { $checked = 'checked ' }
         $output
             .= qq!<input type="$field->{type}" $input_class $extra name="$field->{name}" value="$value" $checked>$v_lab<br>\n!;
@@ -144,9 +187,40 @@ sub _radiocheck {            # field, input_class, data;
     return $output;
 }
 
+sub _select {    # field, input_class, data;
+    my $self           = shift;
+    my $field          = shift;
+    my $data           = shift;
+    my $replace_fields = shift;
+    my $class          = $self->_class_input($field);
+    my $extra          = $field->{extra} || "";
+    my $id = $field->{id} ? qq!id="$field->{id}"! : qq!id="$field->{name}"!;
+    my @values
+        = $replace_fields
+        ? @{$replace_fields}
+        : @{ $field->{values} };
+    my $default = $field->{default}
+        ? do {
+        if   ($data) {undef}
+        else         { $field->{default} }
+        }
+        : undef;
+    my $output = qq|<SELECT name="$field->{name}" $id $extra $class>\n|;
+    foreach my $val (@values) {
+        my ( $value, $v_lab ) = ( split( /\:/, $val ), $val );
+        my $selected = '';
+        if    ( $data eq $value )    { $selected = 'selected ' }
+        elsif ( $default eq $value ) { $selected = 'selected ' }
+        $output .= qq| <option value="$value" $selected>$v_lab</option>\n|;
+    }
+    $output .= '</SELECT>';
+    return $output;
+}
+
 sub generate {
-    my $self = shift;
-    my $data = shift;
+    my $self    = shift;
+    my $data    = shift;
+    my $overide = shift;
     unless ( keys %{$data} ) { $data = undef }
     my @generated = ();
     foreach my $field ( @{ $self->{FormMap} } ) {
@@ -154,8 +228,15 @@ sub generate {
         if ( $field->{type} eq 'radio' || $field->{type} eq 'checkbox' ) {
             $input = $self->_radiocheck(
                 $field,
-                $self->_class_input($field),
-                $data->{ $field->{name} }
+                $data->{ $field->{name} },
+                $overide->{ $field->{name} },
+            );
+        }
+        elsif ( $field->{type} eq 'select' || $field->{type} eq 'datalist' ) {
+            $input = $self->_select(
+                $field,
+                $data->{ $field->{name} },
+                $overide->{ $field->{name} },
             );
         }
         else {
@@ -171,205 +252,5 @@ sub generate {
     }
     return \@generated;
 }
-
-1;
-
-=head1 NAME
- 
-Form::Diva - Generate HTML5 form label and input fields
-
-=head1 VERSION
-
-version 0.04
-
-=head1 SYNOPSIS
- 
-Generate Form Label and Input Tags from a simple data structure.
-Simplify form code in your views without replacing it without a lot of even
-uglier Perl Code in your Controller. 
-
-Drastically reduce form clutter in your View Templates with small Data Structures.
-
-=head1 DESCRIPTION
-
-Create a new instance of Form::Diva from an array_ref of hash_refs describing each field of your form. The most common attributes have a single letter abbreviation to reduce typing.
-
-Return a similar structure of the label and input attributes ready for inclusion in a web page by your templating system.
-
-=head1 USAGE
-
- use Form::Diva;
-
- my $diva = Form::Diva->new(
-    label_class => 'col-sm-3 control-label',
-    input_class => 'form-control',
-    form        => [
-        { n => 'name', t => 'text', p => 'Your Name', l => 'Full Name' },
-        { name => 'phone', type => 'tel', extra => 'required' },
-        { qw / n email t email l Email c form-email placeholder doormat/},
-        { name => 'myradio', type => 'radio', default => 1,
-           values => [ "1:Yes", "2:No", "3:Maybe" ] },   
-    ],
- );
-
- my $fields = $diva->generate;
- my $filledfields = $diva->generate( $hashref_of_data );
-
-Once you send this to your stash or directly to the templating system the form might look like:
-
-  <form class="form-horizontal well col-md-8" role="form"
-   method="post" action="/form1" name="DIVA1" >  
-
-  <div class="form-group">
-In Template Toolkit:
-  [% FOREACH field IN fields %] {
-    [% field.label %]
-    <div class="col-sm-8">
-        [% field.input %]
-    </div>
-  [% END %]
-Or in Mojo::Template
-  % foreach my $field (@$fields) {
-    <%== $field->{'label'} %>
-    <div class="col-sm-8">
-        <%== $field->{'input'} %>
-    </div>
- % }
-
- </div>
-
- <div class="form-group">
-    <div class="col-sm-offset-3 col-sm-8">
-      <input type="submit" class="btn btn-large btn-primary" 
-      name="submit" value="submit_me" >&nbsp;
-    </div>
- </div>
- </form>
-
-=head1 METHODS
-
-=head2 new
-
-Create a new object from a Data Structure ().
-
-=head2 generate
-
-When called without arguments returns the blank form with placeholders and value set to default or null if there is no default.
-
-When provided an optional hashref it sets values based on the hashref and suppresses placeholder. 
-
-The data returned is in the form of an array reference with a hash reference for the label and input attributes.
-
-=head2 spawn
-
-Not yet implemented. The same as generate but specify which fields to include and or change field order.
-
-=head1 The Form::Diva Data Structure
-
- { label_class => 'some class in your css',
-   input_class => 'some class in your css',
-   form        => [
-        { name => 'some_field_name', ... },
-        ...
-   ],
- }
-
-=head2 label_class, input_class
-
-Specify the contents the label's class attribute and the input's class attribute. The input_class can be over-ridden for a single field by using the c/class attribute in a field definition.
-
-=head2 form
-
-Form::Diva knows about the most frequently needed attributes in HTML5 label and input tags. The attribute extra is provided to handle valueless attributes like required and attributes that are not explicitly supported by Form::Diva. Each supported tag has a single character shortcut. When no values in a field definition require spaces the shortcuts make it extremely compact to describe a field using qw/. 
-
-The only required value in a field definition is name. When not specified type defaults to text. 
-
-Multivalued fields are not currently supported, but may be in the future.
-
-Supported attributes and their shortcuts
-
- c       class        over-ride input_class for this field
- d       default      sets value for an empty form
- e,x     extra        any other attribute(s)
- i       id           defaults to name
- l       label        defaults to ucfirst(name)
- n       name         field name -- required
- p       placeholder  placeholder to show in an empty form
- t       type         checkbox, radio, textarea or any input types
-                      type defaults to text input type
- v       values       for radio and checkbox inputs only
-
-=head2 extra attribute
-
-The contents of extra are placed verbatim in the input tag. Use for HTML5 attributes that have no value such as disabled and any of the other attributes you may wish to use in your forms that have not been implemented, you will need to type out attribute="something" if it is not valueluess.
-
-=head3 Common Attributes with no Value
-
-B<disabled>, B<readonly>, B<required>
-
-Should be placed in the extra field when needed.
-
-=head2 TextArea
-
-TextArea fields are handled the same as the text type. 
-
-=head2 Select
-
-The select input type didn't make it into the initial release but is at the top of the list for features to implement.
-
-=head2 Radio Button and CheckBox
-
-Radio Buttons and CheckBoxes are very similar to each other, and take an extra attribute 'values'. Form::Diva does not currently support multi-valued Radio Buttons and CheckBoxes, if a record's data has multiple values only one will be selected in the form.
-
-=head3 values
-
-For CheckBoxes the values attribute is just the values of the check boxes. If value is set and matches one of the values it will be checked. 
-
-  { type => 'checkbox',
-    name => 'mycheckbox',
-    values => [ 'Miami', 'Chicago', 'London', 'Paris' ] }
-
-For RadioButtons the values attribute is a number and text seperated by a colon. When the form is submitted just the number will be returned.
-
-  { t => 'radio',
-    n => 'myradio',
-    v => [ '1:New York', '2:Philadelphia', '3:Boston' ] }
-
-=head1 AUTHOR
-
-John Karr, C<brainbuz at brainbuz.org>
-
-=head1 BUGS
-
-Please report any bugs or feature requests through the web interface at L<https://bitbucket.org/brainbuz/formdiva/issues>.  I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Form::Diva
-
-You can also look for information at:
-
-=head1 ACKNOWLEDGEMENTS
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2014 John Karr.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see L<http://www.gnu.org/licenses/>.
-
-=cut
 
 1;
---
+++ t/180_select.t
@@ -0,0 +1,129 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Test::More;
+use 5.020;
+use Storable qw(dclone);
+
+use_ok('Form::Diva');
+
+=pod Test Select Inputs
+
+=cut
+
+my $select1 = Form::Diva->new(
+    form_name   => 'SELECT1',
+    label_class => 'testclass',
+    input_class => 'form-control',
+    form        => [
+        {   n => 'selecttest',
+            t => 'select',
+            v => [qw /usa:American uk:English can:Canadian/],
+        },
+    ],
+);
+
+my $select2 = Form::Diva->new(
+    form_name   => 'SELECT2',
+    label_class => 'testclass',
+    input_class => 'form-control',
+    form        => [
+        {   n => 'empty',
+            t => 'select',
+            v => [],
+        },
+    ],
+);
+
+my $select3 = Form::Diva->new(
+    form_name   => 'SELECT3',
+    label_class => 'testclass',
+    input_class => 'form-control',
+    form        => [
+        {   name    => 'checktest',
+            type    => 'select',
+            default => 'French',
+            values  => [
+                qw /Argentinian American English Canadian French Irish Russian/
+            ]
+        },
+    ],
+);
+
+my ($newform) = &Form::Diva::_expandshortcuts( $select1->{form} );
+
+is( $newform->[0]{type}, 'select', 'check _expandshortcuts type is select' );
+
+my $input_select3_default = 
+ q |<SELECT name="checktest" id="checktest"  class="form-control">
+ <option value="Argentinian" >Argentinian</option>
+ <option value="American" >American</option>
+ <option value="English" >English</option>
+ <option value="Canadian" >Canadian</option>
+ <option value="French" selected >French</option>
+ <option value="Irish" >Irish</option>
+ <option value="Russian" >Russian</option>
+</SELECT>|;
+
+unlike( $select1->_select( $select1->{form}[0], undef ),
+    qr/selected/,
+    'select1 does not have a default, with no data nothing is selected' );
+my $uk_selected = $select1->_select( $select1->{form}[0], 'uk' );
+like(
+    $uk_selected,
+    qr/uk" selected/,
+    'select1 with uk as data English is now selected'
+);
+like(
+    $uk_selected,
+    qr/usa" >American/,
+    'select1 with uk as data "usa">American has tag and not selected'
+);
+
+my $empty_input_nodata = 
+    qq|<SELECT name="empty" id="empty"  class="form-control">\n</SELECT>|;
+is( $select2->_select( $select2->{form}[0] ) ,
+    $empty_input_nodata ,
+    'select2 has no values provided and returns with no option elements');
+my $select2_no_data = $select2->generate ;
+is( $select2_no_data->[0]{label}, 
+    '<LABEL for="empty" class="testclass">Empty</LABEL>',
+    'Check the label on the empty one');
+# remove extra space because generate does.
+$empty_input_nodata =~ s/\s//g;
+my $generated_empty_input = $select2_no_data->[0]{input};
+$generated_empty_input =~ s/\s//g;
+is( $generated_empty_input, 
+    $empty_input_nodata,
+    'Generate returned input of a few tests ago, with some space removed' );
+
+my $input3a = $select3->_select( $select3->{form}[0], undef, );
+is( $input3a, $input_select3_default, 
+    'A select with different labels than values.' );
+
+my $over_ride2 = $select2->_select( 
+    $select2->{form}[0], undef, [ qw / yellow orange red / ] );
+like( $over_ride2, qr/red/, 
+    'Empty Select with Override now has one of the new vaues' )   ;
+unlike( $over_ride2, qr/selected/, 
+    'Empty Select with Override has no selected because it was given undef' )   ;
+
+my $over_ride3 = $select3->_select( 
+    $select3->{form}[0], 'pear', [ qw / apple orange pear / ] );
+unlike ( $over_ride3, qr/French/, 
+    'Select with Override does not contain an original option value');
+like( $over_ride3, qr/apple/, 
+    'Select with Override does contain one of the new values' )   ;
+like( $over_ride3, qr/<option value="pear" selected >/,
+    'pear is selected in the Override select');
+
+my $over_ride4 = $select3->generate( 
+    { checktest  => 'banana' , pet => 'poodle' },
+    { checktest => [ qw / banana grape peach plum / ] } );
+
+like( $over_ride4->[0]{input} , qr/banana/,
+    'banana is in the new list from generate');
+unlike( $over_ride4->[0]{input} , qr/Canadian/, 'Canadian has been removed' );
+
+
+done_testing();
